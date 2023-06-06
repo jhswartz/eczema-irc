19e7d4ba0ad28a52f6a4f198c86f1e74304febed
Justin Swartz <justin.swartz@risingedge.co.za>
2023-06-06 15:46:39 +0200

    eczema/irc 0.2

--- core.js
+++ core.js
@@ -1089,15 +1089,6 @@ CODE: >=
   system.data.push(x >= y);
 ;
 
-
-\\ String
-
-: MERGE { count glue -- string }
-  count :ARRAY glue JOIN ;
-
-: SPLIT { string delimiter -- array }
-  delimiter 1 "split" string METHOD ;
-
 `);
 system.parse(`
 
@@ -1153,6 +1144,9 @@ CODE: DOCUMENT
 : STYLE? { element -- style }
   "style" element ? ;
 
+: COLOUR? { element -- colour }
+  "color" element STYLE? ;
+
 : HIDE { element -- }
   "none" "display" element STYLE? ! ;
 
@@ -1173,123 +1167,13 @@ CODE: DOCUMENT
   0 "focus" element METHOD ;
 
 `);
-system.parse(`
-
-PUBLISH CANVAS
-
-\\ Canvas
-
-: DIMENSIONS! { width height canvas -- }
-  width "width" canvas ! height "height" canvas ! ;
-
-: DIMENSIONS? { canvas -- width height }
-  "width" canvas ? "height" canvas ? ;
- 
-: CANVAS { width height -- canvas }
-  "canvas" CREATE-ELEMENT { canvas }
-  width height canvas DIMENSIONS! canvas ;
-
-
-\\ Context 
-
-: GET-CONTEXT ( type canvas -- context )
-  1 "getContext" ROT METHOD ;
-
-: 2D ( canvas -- context )
-  "2d" SWAP GET-CONTEXT ;
-
-
-\\ Style
-
-: FILL-STYLE ( value context -- )
-  "fillStyle" SWAP ! ;
-
-: STROKE-STYLE ( value context -- )
-  "strokeStyle" SWAP ! ;
-
-
-\\ Rectangles
-
-: FILL-RECT ( x y width height context -- )
-  4 "fillRect" ROT METHOD ;
-
-: STROKE-RECT ( x y width height context -- )
-  4 "strokeRect" ROT METHOD ;
-
-: CLEAR-RECT ( x y width height context -- )
-  4 "clearRect" ROT METHOD ;
-
-
-\\ Text
-
-: FONT! ( font-face context -- )
-  "font" SWAP ! ;
-
-: TEXT-ALIGN! ( alignment context -- )
-  "textAlign" SWAP ! ;
-
-: TEXT-BASELINE! ( alignment context -- )
-  "textBaseline" SWAP ! ;
-
-: FILL-TEXT ( text x y context -- )
-  3 "fillText" ROT METHOD ;
-
-: STOKE-TEXT ( text x y context -- )
-  3 "strokeText" ROT METHOD ;
-
-
-\\ Paths
-
-: BEGIN-PATH ( context -- )
-  0 "beginPath" ROT METHOD ;
-
-: CLOSE-PATH ( context -- )
-  0 "closePath" ROT METHOD ;
-
-: FILL ( context -- )
-  0 "fill" ROT METHOD ;
-
-: STROKE ( context -- )
-  0 "stroke" ROT METHOD ;
-
-: CLIP ( context -- )
-  0 "clip" ROT METHOD ;
-
-: MOVE-TO ( x y context -- )
-  2 "moveTo" ROT METHOD ;
-
-: LINE-TO ( x y context -- )
-  2 "lineTo" ROT METHOD ;
-
-: SET-LINE-DASH ( segments context -- )
-  1 "setLineDash" ROT METHOD ;
-
-: QUADRATIC-CURVE-TO ( cpx cpy x y context -- )
-  4 "quadraticCurveTo" ROT METHOD ;
-
-: BEZIER-CURVE-TO ( cp1x cp1y cp2x cp2y x y context -- )
-  6 "bezierCurveTo" ROT METHOD ;
-
-: ARC-TO ( x1 y1 x2 y2 radius context -- )
-  5 "arcTo" ROT METHOD ;
-
-: ARC ( x y radius startAngle endAngle anticlockwise context -- )
-  6 "arc" ROT METHOD ;
-
-: RECT ( x y w h context -- )
-  4 "rect" ROT METHOD ;
-
-: IS-POINT-IN-PATH ( x y context -- )
-  2 "isPointInPath" ROT METHOD ;
-
-`);
 system.console.error = function(error) {
   system.data.push(error);
   system.interpret("ERROR");
 }
 
-system.console.write = function(text, type = "default-output") {
-  system.data.push(text, type);
+system.console.write = function(text, colour = "#ccc") {
+  system.data.push(text, colour);
   system.interpret("WRITE");
 };
 
@@ -1313,10 +1197,10 @@ PUBLISH CONSOLE
 VARIABLE LineIndex
 -1 LineIndex !
 
-"timestamp-output" VALUE TimestampOutput
-"default-output"   VALUE DefaultOutput
-"input-output"     VALUE InputOutput
-"error-output"     VALUE ErrorOutput
+"#2a9" VALUE TimestampOutput
+"#ccc" VALUE DefaultOutput
+"#6ec" VALUE InputOutput
+"#ec2" VALUE ErrorOutput
 
 "#view"    QUERY-SELECTOR VALUE #view
 "#output"  QUERY-SELECTOR VALUE #output
@@ -1328,9 +1212,9 @@ VARIABLE LineIndex
 
 \\ Input / Output
 
-: WRITE { text class -- }
+: WRITE { text colour -- }
   "p" CREATE-ELEMENT { line }
-  class line CLASS!
+  colour line COLOUR? !
   text line APPEND
   line #output PREPEND ;
 
@@ -1417,3 +1301,6 @@ VARIABLE LineIndex
 #input FOCUS
 
 `);
+
+system.console.write('"слава україні! https://war.ukraine.ua/support-ukraine/"', "#fd0");
+system.console.write('"россия будет свободной! https://legionliberty.army/"', "#5cd");
